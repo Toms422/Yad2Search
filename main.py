f5bdbc708101b1e7c700c5ef035690f0d60aee8a
Toms422 <96585447+Toms422@users.noreply.github.com>
2024-03-12 14:45:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from io import BytesIO
 
 
 async def send_message_async(bot, chat_id, message_text):
-    await bot.send_message(chat_id=chat_id, text=message_text,parse_mode='Markdown')
+    await bot.send_message(chat_id=chat_id, text=message_text, parse_mode='Markdown')
     print("Message sent successfully!")
 
 
-async def main(params,neighborhood,bot,chat_id_Tom,chat_id_Lee):
-    TotalCheck=False
+async def main(params, neighborhood, bot, chat_id_Tom, chat_id_Lee):
+    TotalCheck = False
     json_file_path = 'unique_date_added.json'
     try:
         with open(json_file_path, 'r') as json_file:
@@ -53,7 +53,6 @@ async def main(params,neighborhood,bot,chat_id_Tom,chat_id_Lee):
         time.sleep(1)
         response = requests.get(final_url, headers=headers)
 
-
         if response.status_code == 200 and response.content:
             try:
                 data = response.json()
@@ -67,15 +66,14 @@ async def main(params,neighborhood,bot,chat_id_Tom,chat_id_Lee):
                                 TotalCheck = True
                     except (KeyError, ValueError):
                         pass
-             except (KeyError, ValueError):
-                    pass          
+            except (KeyError, ValueError):
+                pass
         else:
-            print("Invalid or empty response received.")           
-            
-    if count>0:
+            print("Invalid or empty response received.")
+
+    if count > 0:
         await send_message_async(bot, chat_id_Tom, f' דירות חדשות בשכונת *{neighborhood}*: {count}')
         await send_message_async(bot, chat_id_Lee, f' דירות חדשות בשכונת *{neighborhood}*: {count}')
-
 
     for i in range(1):
         base_url = "https://gw.yad2.co.il/feed-search-legacy/realestate/rent"
@@ -87,7 +85,7 @@ async def main(params,neighborhood,bot,chat_id_Tom,chat_id_Lee):
         # Construct the final URL
         final_url = f"{base_url}?{encoded_params}"
 
-        #url = f'https://gw.yad2.co.il/feed-search-legacy/realestate/rent?topArea=2&area=3&city=8600&neighborhood=1647&rooms=2-4&price=0-7000&balcony=1&squaremeter=65--1&page={i}&forceLdLoad=true'
+        # url = f'https://gw.yad2.co.il/feed-search-legacy/realestate/rent?topArea=2&area=3&city=8600&neighborhood=1647&rooms=2-4&price=0-7000&balcony=1&squaremeter=65--1&page={i}&forceLdLoad=true'
         time.sleep(1)
         response = requests.get(final_url, headers=headers)
 
@@ -97,53 +95,58 @@ async def main(params,neighborhood,bot,chat_id_Tom,chat_id_Lee):
                 # Process the JSON data as needed
                 for d in data['data']['feed']['feed_items']:
                     try:
-                        neighborhood =d['neighborhood']
+                        neighborhood = d['neighborhood']
                     except KeyError:
                         neighborhood = None
                     try:
-                        if d['feed_source']=="private": # דירות לא מתיווך
+                        if d['feed_source'] == "private":  # דירות לא מתיווך
                             Address = d['title_1']
-                            price=d['price']
-                            details=d['row_3'][0]+", "+d['row_3'][1]+", "+d['row_3'][2]
+                            price = d['price']
+                            details = d['row_3'][0] + ", " + d['row_3'][1] + ", " + d['row_3'][2]
                             date_added = d['date_added']
                             print(date_added)
-                            Addid="https://www.yad2.co.il/item/"+d['id']
+                            Addid = "https://www.yad2.co.il/item/" + d['id']
                             if date_added not in unique_date_added:
                                 unique_date_added.add(date_added)
-                                #apartment=d['date_added'], d['date'], d['price'], d['title_1'], d['line_2'], d['line_1'], d['line_3'],neighborhood
-                                await send_message_async(bot, chat_id_Tom,f' *כתובת*: {Address}, *מחיר*: {price}. *פרטים נוספים*:  {details} . [קישור למודעה]({Addid}) ')
-                                await send_message_async(bot, chat_id_Lee,f' *כתובת*: {Address}, *מחיר*: {price}. *פרטים נוספים*:  {details} . [קישור למודעה]({Addid}) ')
-                                await bot.send_location(chat_id=chat_id_Tom, latitude=d['coordinates']['latitude'], longitude=d['coordinates']['longitude'])
-                                await bot.send_location(chat_id=chat_id_Lee, latitude=d['coordinates']['latitude'], longitude=d['coordinates']['longitude'])
-    
-    
+                                # apartment=d['date_added'], d['date'], d['price'], d['title_1'], d['line_2'], d['line_1'], d['line_3'],neighborhood
+                                await send_message_async(bot, chat_id_Tom,
+                                                         f' *כתובת*: {Address}, *מחיר*: {price}. *פרטים נוספים*:  {details} . [קישור למודעה]({Addid}) ')
+                                await send_message_async(bot, chat_id_Lee,
+                                                         f' *כתובת*: {Address}, *מחיר*: {price}. *פרטים נוספים*:  {details} . [קישור למודעה]({Addid}) ')
+                                await bot.send_location(chat_id=chat_id_Tom, latitude=d['coordinates']['latitude'],
+                                                        longitude=d['coordinates']['longitude'])
+                                await bot.send_location(chat_id=chat_id_Lee, latitude=d['coordinates']['latitude'],
+                                                        longitude=d['coordinates']['longitude'])
+
                                 media_items = []
                                 check = False
                                 for image in d['images']:
-                                    image_url=d['images'][image]['src']
+                                    image_url = d['images'][image]['src']
                                     responseImage = requests.get(image_url)
                                     image_bytes = BytesIO(responseImage.content)
-                                    #await bot.send_photo(chat_id=chat_id, photo=image_bytes)
+                                    # await bot.send_photo(chat_id=chat_id, photo=image_bytes)
                                     media_items.append(InputMediaPhoto(media=image_bytes))
-                                    check=True
-                                if check==True:
+                                    check = True
+                                if check == True:
                                     await bot.send_media_group(chat_id=chat_id_Tom, media=media_items)
                                     await bot.send_media_group(chat_id=chat_id_Lee, media=media_items)
-                    except (KeyError,ValueError):
+                    except (KeyError, ValueError):
                         pass
-            except (KeyError,ValueError):
-                    pass    
-        else:
-            print("Invalid or empty response received.")
-            print(f"Request failed with status code: {response.status_code}")
+            except (KeyError, ValueError):
+                pass
+    else:
+        print("Invalid or empty response received.")
+        print(f"Request failed with status code: {response.status_code}")
 
-        # Save the updated unique_date_added to the JSON file
-        with open(json_file_path, 'w') as json_file:
-            json.dump(list(unique_date_added), json_file)
+    # Save the updated unique_date_added to the JSON file
+    with open(json_file_path, 'w') as json_file:
+        json.dump(list(unique_date_added), json_file)
+
 
     return TotalCheck
 if __name__ == "__main__":
     import asyncio
+
     loop = asyncio.get_event_loop()
 
     bot_token = '6582907880:AAEgdSSbRRYzsaoMHNxyl5Jsvl8MQYJq53A'
@@ -155,18 +158,23 @@ if __name__ == "__main__":
 
     # Define property search parameters for different neighborhoods
     neighborhoods_params = [
-        {'name': "הראשונים-רג", 'topArea': 2, 'area': 3, 'city': 8600, 'rooms': '2.5-4', 'price': '0-7000','balcony': 1, 'neighborhood': 1647, 'squaremeter': '65--1', 'forceLdLoad': True},
-        #{'name': "החשמונאים-רג", 'topArea': 2, 'area': 3, 'city': 8600, 'rooms': '2.5-4', 'price': '0-7000','balcony': 1, 'neighborhood': 1477, 'squaremeter': '65--1', 'forceLdLoad': True},
-        {'name': "החרוזים-רג", 'topArea': 2, 'area': 3, 'city': 8600, 'rooms': '2.5-4', 'price': '0-7000', 'balcony': 1,'neighborhood': 327, 'squaremeter': '65--1', 'forceLdLoad': True},
-        {'name': "בורכוב-גבעתיים", 'topArea': 2, 'area': 3, 'city': 6300, 'rooms': '2.5-4', 'price': '0-7000','balcony': 1, 'neighborhood': 355, 'squaremeter': '65--1', 'forceLdLoad': True},
-        {'name': "הלה-גבעתיים", 'topArea': 2, 'area': 3, 'city': 6300, 'rooms': '2.5-4', 'price': '0-7000','balcony': 1, 'neighborhood': 991510, 'squaremeter': '65--1', 'forceLdLoad': True},
-        {'name': "ארלוזורוב-גבעתיים", 'topArea': 2, 'area': 3, 'city': 6300, 'rooms': '2.5-4', 'price': '0-7000','balcony': 1, 'neighborhood': 1642, 'squaremeter': '65--1', 'forceLdLoad': True},
-        #{'name': "גבעת רמבם-גבעתיים", 'topArea': 2, 'area': 3, 'city': 6300, 'rooms': '2.5-4', 'price': '0-7000','balcony': 1, 'neighborhood': 1643, 'squaremeter': '65--1', 'forceLdLoad': True},
-        #{'name': "קריית יוסף-גבעתיים", 'topArea': 2, 'area': 3, 'city': 6300, 'rooms': '2.5-4', 'price': '0-7000','balcony': 1, 'neighborhood': 1644, 'squaremeter': '65--1', 'forceLdLoad': True},
-        #{'name': "נחלת יצחק-תל אביב", 'topArea': 2, 'area': 1, 'city': 5000, 'rooms': '2.5-4', 'price': '0-7000','balcony': 1, 'neighborhood': 317, 'squaremeter': '65--1', 'forceLdLoad': True},
-        #{'name': "מונטיפיורי-תל אביב", 'topArea': 2, 'area': 1, 'city': 5000, 'rooms': '2.5-4', 'price': '0-7000','balcony': 1, 'neighborhood': 485, 'squaremeter': '65--1', 'forceLdLoad': True},
-        #{'name': "צפון ישן-תל אביב", 'topArea': 2, 'area': 1, 'city': 5000, 'rooms': '2.5-4', 'price': '0-7000','balcony': 1, 'parking': 1, 'neighborhood': 204, 'squaremeter': '65--1', 'forceLdLoad': True},
-        #{'name': "בבלי-תל אביב", 'topArea': 2, 'area': 1, 'city': 5000, 'rooms': '2.5-4', 'price': '0-7000','balcony': 1, 'parking': 1, 'neighborhood': 1518, 'squaremeter': '65--1', 'forceLdLoad': True},
+        {'name': "הראשונים-רג", 'topArea': 2, 'area': 3, 'city': 8600, 'rooms': '2.5-4', 'price': '0-7000',
+         'balcony': 1, 'neighborhood': 1647, 'squaremeter': '65--1', 'forceLdLoad': True},
+        # {'name': "החשמונאים-רג", 'topArea': 2, 'area': 3, 'city': 8600, 'rooms': '2.5-4', 'price': '0-7000','balcony': 1, 'neighborhood': 1477, 'squaremeter': '65--1', 'forceLdLoad': True},
+        {'name': "החרוזים-רג", 'topArea': 2, 'area': 3, 'city': 8600, 'rooms': '2.5-4', 'price': '0-7000', 'balcony': 1,
+         'neighborhood': 327, 'squaremeter': '65--1', 'forceLdLoad': True},
+        {'name': "בורכוב-גבעתיים", 'topArea': 2, 'area': 3, 'city': 6300, 'rooms': '2.5-4', 'price': '0-7000',
+         'balcony': 1, 'neighborhood': 355, 'squaremeter': '65--1', 'forceLdLoad': True},
+        {'name': "הלה-גבעתיים", 'topArea': 2, 'area': 3, 'city': 6300, 'rooms': '2.5-4', 'price': '0-7000',
+         'balcony': 1, 'neighborhood': 991510, 'squaremeter': '65--1', 'forceLdLoad': True},
+        {'name': "ארלוזורוב-גבעתיים", 'topArea': 2, 'area': 3, 'city': 6300, 'rooms': '2.5-4', 'price': '0-7000',
+         'balcony': 1, 'neighborhood': 1642, 'squaremeter': '65--1', 'forceLdLoad': True},
+        # {'name': "גבעת רמבם-גבעתיים", 'topArea': 2, 'area': 3, 'city': 6300, 'rooms': '2.5-4', 'price': '0-7000','balcony': 1, 'neighborhood': 1643, 'squaremeter': '65--1', 'forceLdLoad': True},
+        # {'name': "קריית יוסף-גבעתיים", 'topArea': 2, 'area': 3, 'city': 6300, 'rooms': '2.5-4', 'price': '0-7000','balcony': 1, 'neighborhood': 1644, 'squaremeter': '65--1', 'forceLdLoad': True},
+        # {'name': "נחלת יצחק-תל אביב", 'topArea': 2, 'area': 1, 'city': 5000, 'rooms': '2.5-4', 'price': '0-7000','balcony': 1, 'neighborhood': 317, 'squaremeter': '65--1', 'forceLdLoad': True},
+        # {'name': "מונטיפיורי-תל אביב", 'topArea': 2, 'area': 1, 'city': 5000, 'rooms': '2.5-4', 'price': '0-7000','balcony': 1, 'neighborhood': 485, 'squaremeter': '65--1', 'forceLdLoad': True},
+        # {'name': "צפון ישן-תל אביב", 'topArea': 2, 'area': 1, 'city': 5000, 'rooms': '2.5-4', 'price': '0-7000','balcony': 1, 'parking': 1, 'neighborhood': 204, 'squaremeter': '65--1', 'forceLdLoad': True},
+        # {'name': "בבלי-תל אביב", 'topArea': 2, 'area': 1, 'city': 5000, 'rooms': '2.5-4', 'price': '0-7000','balcony': 1, 'parking': 1, 'neighborhood': 1518, 'squaremeter': '65--1', 'forceLdLoad': True},
     ]
 
     # Check for new properties in each neighborhood
